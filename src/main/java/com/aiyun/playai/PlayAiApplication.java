aa40605e4b109b384badace4e96762eb64500374
yuxinbai <784724280@qq.com>
2026-01-26 09:12:34 +0800

    探索 玩一下ai

---
+++ src/main/java/com/aiyun/playai/PlayAiApplication.java
@@ -0,0 +1,12 @@
+package com.aiyun.playai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayAiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlayAiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/aiyun/playai/PlayAiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.aiyun.playai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayAiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
